bc2d83bbd263bb9d08969b45c6419da8ac2015c3
giovanny-garcia <135080402+giovanny-garcia@users.noreply.github.com>
2023-06-20 22:52:32 +0000

    fixes

--- src/homework/04_repetition/dna.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-
-using std::string;
-
-int get_gc_content(const string reference);
-int get_reverse_string();
-int get_dna_conmplement();
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,15 +1,14 @@
 //write include statements
-#include "dna.h"
-
+#include <iostream>
+#include <string>
 using std::cout; using std::cin;
 
 //write using statements
 
 int main() 
 {
-	string DNA = "";
+	std::string DNA = "";
 	cout<< "Enter the DNA string: \n";
 	cin>>DNA;
-	get_gc_content(DNA);
 	return 0;
 }
